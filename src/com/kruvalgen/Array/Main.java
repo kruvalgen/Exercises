e53a5bb672303b30cf5449969efb8a7f292c2f94
kruvalgen <kruvalgen@yandex.ru>
2021-04-25 13:35:19 +0300

    Add task conditions

--- src/com/kruvalgen/Array/Main.java
+++ src/com/kruvalgen/Array/Main.java
@@ -1,7 +1,7 @@
 package com.kruvalgen.Array;
 
 import java.util.Arrays;
-
+// int arr[] = {1,1, 2, 1, 1,1 ,1,4, 5,6,7} посчитать количество каждой цифры и вывести на экран (цифра 1 встречается n раз)
 public class Main {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 1, 2, 1, 1, 1, 1, 4, 5, 6, 7};
