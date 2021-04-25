dec53db529e579d86c64f617400a83fc43f6d58e
kruvalgen <kruvalgen@yandex.ru>
2021-04-25 22:38:31 +0300

    add method printResult

--- src/com/kruvalgen/Array/Main.java
+++ src/com/kruvalgen/Array/Main.java
@@ -1,21 +1,47 @@
 package com.kruvalgen.Array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 // int arr[] = {1,1, 2, 1, 1,1 ,1,4, 5,6,7} посчитать количество каждой цифры и вывести на экран (цифра 1 встречается n раз)
 public class Main {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 1, 2, 1, 1, 1, 1, 4, 5, 6, 7};
 
-        Arrays.sort(arr);
+//        Arrays.sort(arr);
+//
+//        int j = arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if(arr[i] == arr[i + 1]){
+//                j++;
+//            }else{
+//                System.out.println("The number 1 occurs " + j + " times");
+//                break;
+//            }
+//        }
+        Map<Integer, Integer> arrMap = new HashMap<>();
 
-        int j = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == arr[i + 1]){
-                j++;
-            }else{
-                System.out.println("The number 1 occurs " + j + " times");
-                break;
-            }
+
+        for (int i : arr) {
+            int newValue = arrMap.getOrDefault(i, 0) + 1;
+            arrMap.put(i, newValue);
+        }
+
+        printResult(arrMap);
+
+
+    }
+
+    public static void printResult (Map<Integer, Integer> arrMap){
+        for (Map.Entry<Integer, Integer> pair : arrMap.entrySet()){
+            Integer value = pair.getValue();
+            Integer key = pair.getKey();
+            System.out.println("Цифра " + key + " встечается " + value + " раз ");
+
+
         }
     }
+
+
 }
